a573770477521385c80ab22368c941350d05c9e6
Björn Ramberg <bjorn.ramberg@ikea.com>
2019-08-12 21:50:26 +0200

    init commit for api code with vendor set

--- api.go
+++ api.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	var apirouter = mux.NewRouter()
+	apirouter.HandleFunc("/health", health).Methods("GET")
+
+	fmt.Println("API srv up and running")
+	log.Fatal(http.ListenAndServe(":3000", apirouter))
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("alive")
 }
